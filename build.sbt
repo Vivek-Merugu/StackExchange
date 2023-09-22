784f3d9713b33b4a80527e83a47e60069f65d5ed
vmeru328 <vmeru328@marriott.com>
2023-09-22 23:08:08 +0530

    test case-1:Straight for forward -> second commit

--- build.sbt
+++ build.sbt
@@ -2,3 +2,11 @@ organization := "com.vivek"
 scalaVersion := "2.12.15"
 name := "StackExchange"
 version := "0.0.1-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+
+  "org.apache.spark" %% "spark-core" % "3.2.0" % "provided",
+  "org.apache.spark" %% "spark-sql" % "3.2.0" % "provided",
+  "org.apache.spark" %% "spark-avro" % "3.2.0" % "provided",
+  "org.apache.hadoop" % "hadoop-client" % "3.2.1" % "provided",
+  "org.apache.hadoop" % "hadoop-yarn-client" % "3.2.1" % "provided")
---
+++ src/main/scala/StackExchange.scala
@@ -0,0 +1,79 @@
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+
+
+
+object StackExchange {
+
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    println("=====hello world==========")
+
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
+
+    // Initialize Spark session
+    val spark: SparkSession = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("FXEngine")
+      .getOrCreate()
+
+    // Read the CSV file containing orders
+    val ordersDF = spark.read
+      .option("header", "false")
+      .option("inferSchema", "true")
+      .csv("src\\main\\resources\\exampleOrders.csv")
+      .toDF("OrderID", "UserName", "OrderTime", "OrderType", "Quantity", "Price").withColumn("status",lit("open"))
+
+    ordersDF.show(truncate=false)
+
+    val buyOrder = ordersDF.filter("OrderType=='BUY'").select(col("OrderID").alias("BOrderID"),col("UserName").alias("BUserName"),
+      col("OrderTime").alias("BOrderTime"),col("OrderType").alias("BOrderType"),col("Quantity"),col("Price").alias("BPrice"),
+      col("status").alias("BStatus"))
+    buyOrder.show(truncate = false)
+
+    val sellOrder = ordersDF.filter("OrderType=='SELL'").select(col("OrderID").alias("SOrderID"),col("UserName").alias("SUserName"),
+      col("OrderTime").alias("SOrderTime"),col("OrderType").alias("SOrderType"),col("Quantity"),col("Price").alias("SPrice"),
+      col("status").alias("SStatus"))
+    sellOrder.show(truncate = false)
+
+
+    val matchOrdersBasedOnQuantity = buyOrder.join(sellOrder,Seq("Quantity"),"inner")
+    matchOrdersBasedOnQuantity.printSchema()
+
+    matchOrdersBasedOnQuantity.show(truncate = false)
+
+    val matchOrdersBasedOnQuantity_OrderTime = matchOrdersBasedOnQuantity.withColumn("OrderTime",
+      when(col("BOrderTime") > col("SOrderTime"), col("BOrderTime"))
+        .otherwise(col("SOrderTime")))
+
+    matchOrdersBasedOnQuantity_OrderTime.show(truncate = false)
+
+    val matchOrdersBasedOnQuantity_OrderTime_latestOrderID_oldOrderID = matchOrdersBasedOnQuantity_OrderTime.withColumn("latestOrderID",
+      when(col("BOrderID") > col("SOrderID"),col("BOrderID"))
+        .otherwise(col("SOrderID")))
+      .withColumn("oldOrderID",
+        when(col("BOrderID") < col("SOrderID"),col("BOrderID"))
+          .otherwise(col("SOrderID")))
+
+    matchOrdersBasedOnQuantity_OrderTime_latestOrderID_oldOrderID.show(truncate = false)
+
+    val matchOrdersBasedOnQuantity_OrderTime_latestOrderID_oldOrderID_price = matchOrdersBasedOnQuantity_OrderTime_latestOrderID_oldOrderID.withColumn("Price",
+      when(col("SOrderTime")<col("BOrderTime"),col("SPrice"))
+        .otherwise(col("BPrice")))
+
+    val matchOrder = matchOrdersBasedOnQuantity_OrderTime_latestOrderID_oldOrderID_price.select("latestOrderID","oldOrderID","OrderTime","Quantity","Price")
+
+    matchOrder.show(truncate = false)
+
+    matchOrder.write.option("header","false").csv("src\\main\\resources\\outputExampleMatches.csv")
+
+
+  }
+}
